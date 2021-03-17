8deb42457eeeaceefafe995e71c70304b1d4cfd3
Tom Harrington <tph@atomicbird.com>
2021-03-17 16:31:52 -0600

    Initial commit of code used in writing blog post at https://atomicbird.com/blog/core-data-back-up-store/

---
+++ CDMoveDemo/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  CDMoveDemo
+//
+//  Created by Tom Harrington on 5/12/20.
+//  Copyright © 2020 Atomic Bird LLC. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
